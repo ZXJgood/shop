b95c50214246e18467e7f73681cb8909dd9c4339
A_佳爷 <849485731@qq.com>
2017-05-09 21:17:21 +0800

    Initial Commit

---
+++ shop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shop
+//
+//  Created by 晓佳 on 2017/5/9.
+//  Copyright © 2017年 晓佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shop
+//
+//  Created by 晓佳 on 2017/5/9.
+//  Copyright © 2017年 晓佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
